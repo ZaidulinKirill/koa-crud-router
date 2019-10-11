5398c37e8b457f73be3d03bc9c49db351d2f9782
Zaidulin Kirill <zaidulinkirill@gmail.com>
2019-10-11 23:54:24 +0300

    Fix aggregation

--- src/utils/preprocessMongoMatch.js
+++ src/utils/preprocessMongoMatch.js
@@ -3,11 +3,10 @@ import ObjectID from 'bson-objectid';
 export default (model, query = {}) => Object.entries(query)
   .map(([key, value]) => {
     const propertyPath = model.schema.path(key);
-    if (!propertyPath) throw new Error('Invalid property');
-
-    if (propertyPath.instance === 'ObjectID') {
+    if (propertyPath && propertyPath.instance === 'ObjectID') {
       return [key, ObjectID(value)];
     }
+
     return [key, value];
   })
   .reduce((acc, [key, value]) => {
