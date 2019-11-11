8ad1b4d31b4f0aaaa450c23277caa65e206c67f8
Zaidulin Kirill <zaidulinkirill@gmail.com>
2019-11-11 15:27:01 +0300

    Fix filter parser

--- src/utils/parseFilters.js
+++ src/utils/parseFilters.js
@@ -1,15 +1,15 @@
 import ObjectID from 'bson-objectid';
 
 export default filter => JSON.parse(filter, (name, value) => {
-  if (value.startsWith && value.startsWith('_(') && value.endsWith(')')) {
+  if (value && value.startsWith && value.startsWith('_(') && value.endsWith(')')) {
     return ObjectID(value.replace('_(', '').replace(')', ''));
   }
 
-  if (value.startsWith && value.startsWith('_d(') && value.endsWith(')')) {
+  if (value && value.startsWith && value.startsWith('_d(') && value.endsWith(')')) {
     return new Date(value.replace('_d(', '').replace(')', ''));
   }
 
-  if (value.startsWith && value.startsWith('_r(') && value.endsWith(')')) {
+  if (value && value.startsWith && value.startsWith('_r(') && value.endsWith(')')) {
     return new RegExp(value.replace('_r(', '').replace(')', ''));
   }
 
