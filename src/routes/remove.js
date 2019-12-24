be281abec79f7117dbc1c22cf6fe70d6299ddaea
Zaidulin Kirill <zaidulinkirill@gmail.com>
2019-12-24 21:38:06 +0300

    Update

--- src/routes/remove.js
+++ src/routes/remove.js
@@ -1,8 +1,13 @@
+import parseFilters from '../utils/parseFilters';
+
 export default ({
   model, removedKey = 'isRemoved',
 }) => async (ctx) => {
+  const query = (ctx.request.query || {});
   await model.updateOne({
-    ...(ctx.request.query || {}),
+    ...Object.assign({}, ...Object.entries(query).map(([key, value]) => ({
+      [key]: parseFilters(value || '{}'),
+    }))),
     [removedKey]: { $ne: true },
   }, { $set: { [removedKey]: true } });
   ctx.status = 200;
